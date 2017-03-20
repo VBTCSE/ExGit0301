8ca3181c4778d8ce654247d6fa6b190bb9b7dab7
J. M. Hinckley <jhinckley@vbisd.org>
2017-03-20 11:27:05 -0400

    Initial work on the app

---
+++ App0301/App0301/App0301.cpp
@@ -0,0 +1,13 @@
+// App0301.cpp : Defines the entry point for the console application.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello World!\n\n";
+	system("pause");
+	return 0;
+}
+
