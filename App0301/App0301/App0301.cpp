eb8ebfe56b062dc4239d02bab585e9c4a5e59dab
J. M. Hinckley <jhinckley@vbisd.org>
2017-03-20 11:57:15 -0400

    added some more code

--- App0301/App0301/App0301.cpp
+++ App0301/App0301/App0301.cpp
@@ -8,6 +8,11 @@ int main()
 {
 	cout << "Hello World!\n\n";
 	cout << "This is an addition made by a collaborator.\n\n";
+	cout << "Enter a number: ";
+	int x = 0;
+	cin >> x;
+	cout << "\nThe number is: " << x << endl << endl;
+
 	system("pause");
 	return 0;
 }
